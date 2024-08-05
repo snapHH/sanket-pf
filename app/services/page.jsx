c2347c1176f8d5d541903fabc5e7b8cf3a1e20ac
Sanket Hadap <123326672+snapHH@users.noreply.github.com>
2024-08-05 23:28:22 +0530

    Update page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -6,26 +6,26 @@ import { Description } from "@radix-ui/react-dialog";
 const services = [
     {
         num: "01",
-        title: "Web Devleopment",
-        description: " I design and deploy interactive and user-friendly web applications using technologies like ReactJS, NextJS, HTML, CSS, and SQL. My experience includes creating secure e-commerce platforms and comprehensive bus management systems, ensuring seamless user experiences and robust backend functionality.",
+        title: "Full Stack Devleopment",
+        description: " Deployed interactive and user-friendly web applications using technologies like ReactJS, NextJS, HTML, CSS, and SQL. My experience includes creating secure e-commerce platforms and comprehensive bus management systems, ensuring seamless user experiences and robust backend functionality.",
         href: ""
     },
     {
         num: "02",
         title: "Software Development",
-        description: " I developed applications using Python and various frameworks like flask and django. My proficiency in Python-based application development.",
+        description: " Built applications using Python and various frameworks like flask and django. My proficiency in Python-based application development.",
         href: ""
     },
     {
         num: "03",
         title: "Data Science",
-        description: " I deployed predictive models using libraries like pandas, numpy, and matplotlib for data visualization and also have the experience in jupyter notebook and google collab. I harnessed these tools to analyze data effectively, providing valuable insights and contributing to data-driven decision-making processes.",
+        description: " Built predictive models using libraries like pandas, numpy, and matplotlib for data visualization and also have the experience in jupyter notebook and google collab. I harnessed these tools to analyze data effectively, providing valuable insights and contributing to data-driven decision-making processes.",
         href: ""
     },
     {
         num: "04",
         title: "Data Analytics",
-        description: "I also specialized in visualizing complex data sets using tools like Tableau and Power BI. I extracted actionable insights by applying my skills in data manipulation and visualization, aiding businesses in making informed and strategic decisions.",
+        description: "Have experience with visualizing complex data sets using tools like Tableau and Power BI. I extracted actionable insights by applying my skills in data manipulation and visualization, aiding businesses in making informed and strategic decisions.",
         href: ""
     },
 
