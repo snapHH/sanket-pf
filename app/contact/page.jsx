5b210ddffe62f8eedddf2f8c8987769508d38a9b
Sanket Hadap <123326672+snapHH@users.noreply.github.com>
2024-08-05 23:24:50 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
             <div className = "xl:w-[54%] order-2 xl:order-none">
                 <form className = "flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" ref = {form} onSubmit ={sendEmail}>
                     <h3 className = "text-4xl text-accent">Contact here</h3>
-                    <p className ="text-white/60">Hello! I want to kickstart my career as a Software Development Engineer SDE fresh out of school. I want to learn and grow in my career while contributing to your team's success. Let's collaborate and achieve great things together!</p>
+                    <p className ="text-white/60">Hello! I want to kickstart my career as a Software Development Engineer. I am fresh out of school. I want to learn and grow in my career while contributing to your team's success. Let's collaborate and achieve great things together!</p>
                     {/* input */}
                     <div className = "grid grid-cols-1 md:grid-cols-2 gap-6">
                     <Input type = "firstname" placeholder="Firstname" name = "firstname" />
